9bc985bfcc53ae93541af6c1f740ff3513586aad
Marianna Hnatovska <mhnatovs@2-A-6.42heilbronn.de>
2026-02-09 13:07:15 +0100

    delete not necessary code

--- include/vector.h
+++ include/vector.h
@@ -8,13 +8,6 @@ typedef struct	s_vector
 	float	z;
 }				t_vector;
 
-typedef struct s_matrix
-{
-	float rotation[4][4];
-	float position[4][4];
-	float trans[4][4];
-} t_matrix;
-
 t_vector	*new_vector(float x, float y, float z);
 
 #endif
